4f4bb7e09f75f3d37d7fa26bdb5e8b5228ad506d
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2021-05-16 19:28:04 +0100

    Create CustomHasManyRelation.php

---
+++ Relation/CustomHasManyRelation.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Models\Relation;
+
+
+use Illuminate\Support\Str;
+
+trait CustomHasManyRelation
+{
+
+    /**
+     * Create a new model instance for a related model.
+     *
+     * @param  string  $class
+     * @return mixed
+     */
+    protected function newRelatedInstance($class)
+    {
+        return tap(new $class, function ($instance) {
+            if (! $instance->getConnectionName()) {
+                $instance->setConnection($this->connection);
+            }
+        });
+    }
+    /**
+     * Define a one-to-many relationship.
+     *
+     * @param  string  $related
+     * @param  string|null  $foreignKey
+     * @param  string|null  $localKey
+     * @return CustomRelation
+     */
+    public function customHasMany($related, $foreignKey = null, $localKey = null)
+    {
+        $instance = $this->newRelatedInstance($related);
+
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new CustomRelation($instance->newQuery(), $this, $foreignKey, $localKey);
+    }
+}
